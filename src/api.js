0627380a9bba6965c42d2dd66040dd8415597f27
Drazington <stevensguerriero@gmail.com>
2022-11-01 20:11:30 -0400

    initial commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '4e1727ea37msh17ccfdf4d9c636bp11ff62jsn3820ce853608',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "4358ea6470fc20200c360600e3652058";
